3b4c17dc1dbe6a3eaa1bae04831343a8dad60717
renan_rossi <renanrossidias@yahoo.com.br>
2020-03-26 18:58:23 -0300

    Creates home component at project

---
+++ src/app/core/services/api.service.ts
@@ -0,0 +1,49 @@
+import { Injectable, NgModule } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HTTP_INTERCEPTORS
+} from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { tap, catchError } from 'rxjs/internal/operators';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable({ providedIn: 'root' })
+export class ApiService {
+  apiUrl: string;
+  constructor() {
+    this.apiUrl = this.env.dev;
+  }
+
+  private env = {
+    dev: 'https://swapi.co/api/'
+  };
+}
+
+@Injectable()
+class ErrorInterceptor implements HttpInterceptor {
+  constructor(private toastr: ToastrService) {}
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const dupReq = req.clone({
+    });
+    return next.handle(dupReq).pipe(
+      catchError(err => {
+        this.toastr.error(err.error.message);
+        return of(null);
+      })
+    );
+  }
+}
+
+@NgModule({
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true
+    }
+  ]
+})
+export class Interceptor {}
